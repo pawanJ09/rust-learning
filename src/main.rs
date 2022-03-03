c30da4b218a6a26fcb99385faab9ebb0d5cf0ab1
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-03-03 16:24:14 -0600

    Accepting input from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Enter your input: ");
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("You entered: {}", input);
+        },
+        Err(e) => {
+            println!("Error encountered when capturing input {}", e);
+        }
+    }
 }
