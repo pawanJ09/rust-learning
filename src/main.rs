383efee6d54f6b893a1e757b3ffad6b6a94c44c4
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-03-08 16:59:26 -0600

    Adding singleline, multiline, documentation, heading, code comment types

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@
 use std::io::{stdout, stdin, BufWriter};
 use ferris_says::say; // from the dependencies in toml file
 
+/// main function for this crate(module)
 fn main() {
+    //! # Main function
+    //!
+    //! ```
+    //! fn main()
+    //! ```
+    //!
+    //! Reads user name as input and generates message by ferris.
+    //!
+
+    // Creating a mutable string as new
     let mut input = String::new();
     println!("Enter your input: ");
     match stdin().read_line(&mut input) {
         Ok(_) => {
+            /*
+            Here we are using the input and formatting our welcome message.
+            This message will then be used to pass to the ferris-say function.
+             */
             let stdout = stdout();
             let input_message = format!("Hello {}", input);
             let message = String::from(input_message);
@@ -17,6 +32,7 @@ fn main() {
             say(message.as_bytes(), width, &mut writer).unwrap();
         },
         Err(e) => {
+            // Print error if input fails
             println!("Error encountered when capturing input {}", e);
         }
     }
