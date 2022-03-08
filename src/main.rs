a45568528de18b242b9d9e47e486c9dac1cbcf6e
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-03-08 16:40:41 -0600

    Adding ferris-says depdendency

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::io;
+#[allow(unused_imports)]
+
+use std::io::{stdout, stdin, BufWriter};
+use ferris_says::say; // from the dependencies in toml file
 
 fn main() {
     let mut input = String::new();
     println!("Enter your input: ");
-    match io::stdin().read_line(&mut input) {
+    match stdin().read_line(&mut input) {
         Ok(_) => {
-            println!("You entered: {}", input);
+            let stdout = stdout();
+            let input_message = format!("Hello {}", input);
+            let message = String::from(input_message);
+            let width = message.chars().count();
+
+            let mut writer = BufWriter::new(stdout.lock());
+            say(message.as_bytes(), width, &mut writer).unwrap();
         },
         Err(e) => {
             println!("Error encountered when capturing input {}", e);
